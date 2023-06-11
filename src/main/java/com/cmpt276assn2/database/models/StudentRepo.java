c95009166ff0f0fda78d7ca0bbac0d0a3787ed6f
Henry Dinh <dinhhenry@hotmail.com>
2023-06-11 02:26:23 -0700

    adding files

---
+++ src/main/java/com/cmpt276assn2/database/models/StudentRepo.java
@@ -0,0 +1,10 @@
+package com.cmpt276assn2.database.models;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepo extends JpaRepository<Student, Integer>{
+    List<Student> findBySize(int size);
+    List<Student> findByName(String name);
+}
